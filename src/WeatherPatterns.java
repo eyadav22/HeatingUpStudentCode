1a35e909fdb8df3ea307b8bee530ba35f3b4f2ea
emill-y <eishayadav@gmail.com>
2026-01-14 22:25:56 -0800

    Amended Work Log

--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -15,8 +15,14 @@ public class WeatherPatterns {
      * @return the longest run of days with increasing temperatures
      */
     public static int longestWarmingTrend(int[] temperatures) {
-        // TODO: Write your code here!
+        return helper(temperatures, -1, 0);
+    }
 
-        return 0;
+    private static int helper(int[] temperatures, int prevIndex, int currentIndex) {
+        if (currentIndex == temperatures.length) return 0;
+        int skip = helper(temperatures, prevIndex, currentIndex + 1);
+        int take = (prevIndex == -1 || temperatures[currentIndex] > temperatures[prevIndex]) ?
+                1 + helper(temperatures, currentIndex, currentIndex + 1) : 0;
+        return Math.max(skip, take);
     }
 }
